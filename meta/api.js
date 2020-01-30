ba874da528d87e645c71c585cabf18742beaee66
Scott Campbell <stc9486@gmail.com>
2020-01-30 12:53:33 -0800

    starting metadata

---
+++ meta/api.js
@@ -0,0 +1,165 @@
+const NYC = {
+  id: 'nyc',
+  title: 'Big Apple',
+  map: {
+      top;
+      left;
+  },
+  image;
+  audio;
+  action;
+  description: `
+      All the way from Lowell, you made it to NYC!
+      You're a student and a standout on the gridiron in Columbia, not bad for kid from Mill-City.
+      But tonight you stumbled into a haunt on 9th and 3rd and met a cool, cool cat named Ginsberg, he was reading some poem he was writing, Howl.
+      What do you think? Call it a night, get some rest for the game tomorrow, or see what Ginsberg and his crew have planned for the evening?
+  `,
+  choices: [{
+      id: 'rest',
+      description: 'Call it a night',
+      result: 'You did great in the game, but you never got around to writing that book... And now you in Queens with a wife and kids,every winter planning on dusting off that typewriter...',
+      //hp;
+      // this ends the game
+      //gold; 
+  }, {
+      id: 'howl',
+      description: 'Out all night',
+      result: "You go out all night, from place to place, alley to alley, and wind up at some apartment, somewhere in Manhattan, and your new poet friend introduces you to a guy named Neal Cassady. And right as the sun was rising over the East River, Neal said, 'man, forget the game, the road is out there, let's go!",
+      //hp: -50,
+      // THIS PROMPTS THE GAME ON ...
+      //gold: 0
+
+  }]
+};
+
+const Chicago = {
+  id: 'chicago',
+  title: 
+  map: {
+      top;
+      left;
+  },
+  image;
+  audio;
+  action;
+  description: `
+      
+  `,
+  choices: [{
+      id: 
+      description: 
+      result: 
+      //
+      // this ends the game
+      //
+  }, {
+      id: 
+      description: 
+      result: 
+      //hp: 
+      // 
+      //gold:
+
+  }]
+};
+
+const New Orleans = {
+  id: 'new-orleans',
+  title: 
+  map: {
+      top;
+      left;
+  },
+  image;
+  audio;
+  action;
+  description: `
+      
+  `,
+  choices: [{
+      id: 
+      description: 
+      result: 
+      //
+      // this ends the game
+      //
+  }, {
+      id: 
+      description: 
+      result: 
+      //hp: 
+      // 
+      //gold:
+
+  }]
+};
+const Denver = {
+  id: 'denver',
+  title: 
+  map: {
+      top;
+      left;
+  },
+  image;
+  audio;
+  action;
+  description: `
+      
+  `,
+  choices: [{
+      id: 
+      description: 
+      result: 
+      //
+      // this ends the game
+      //
+  }, {
+      id: 
+      description: 
+      result: 
+      //hp: 
+      // 
+      //gold:
+
+  }]
+};
+const San Francisco = {
+  id: 'san-francisco',
+  title: 
+  map: {
+      top;
+      left;
+  },
+  image;
+  audio;
+  action;
+  description: `
+      
+  `,
+  choices: [{
+      id: 
+      description: 
+      result: 
+      //
+      // this ends the game
+      //
+  }, {
+      id: 
+      description: 
+      result: 
+      //hp: 
+      // 
+      //gold:
+
+  }]
+};
+const cities = [
+  NYC,
+  Chicago,
+  New Orleans,
+  Chicago, 
+  Denver,
+  San Francisco,
+];
+
+export default cities;
